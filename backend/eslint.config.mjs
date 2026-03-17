b320812849b4234237e1cd9a590d517361e85612
Nagomu0128 <kyoshidk@gmail.com>
2026-03-18 00:09:22 +0900

    add linter and formtter on backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+];
